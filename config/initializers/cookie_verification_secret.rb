3a602b614ca6c72c160299f6cca9b22816158981
Cristiany <cristyma@gmail.com>
2012-02-17 15:45:39 -0200

    vincula projeto ao mysql

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '1e3efcd1dc60238727097762c38140d050fe1da026923aa56c9a930a65bc6796ee9d329ddcf2f54b129b8726b8e6f8788fb9d374692a92ecb46ad90e24df2ce2';
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlmoxWeb::Application.config.secret_token = '3fa15e91308c56363dd9a2d24da683480c125e7d799b32a1bc9cf8f42980dc70c513b9f58683a82821deb227251b1640229569f3a37649b095dc842315fdf335'
